8551318df1ad717114bbc44d2844ba5887897f05
lemon <lemon@example.com>
2025-11-27 14:18:21 +0300

    commit

--- src/app/verification-success/page.tsx
+++
@@ -1,6 +0,0 @@
-// ai-chat-next/src/app/verification-success/page.tsx
-import VerificationSuccess from "@features/common/VerificationSuccess";
-
-export default function Page() {
-  return <VerificationSuccess />;
-}
